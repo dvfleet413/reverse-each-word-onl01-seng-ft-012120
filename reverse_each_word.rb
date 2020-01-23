7b8e3c736d3d60c50cf3a4c3dec1f299ba9df797
David Van Fleet <dvfleet413@gmail.com>
2020-01-23 03:31:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,4 @@
 def reverse_each_word(string)
-  array = string.split
-  array.collect do |word|
-    word.reverse
-  end
+  result = string.split.collect { |word| word.reverse }
+  result.join(' ')
 end
